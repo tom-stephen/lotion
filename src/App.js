cea12384a02047c616f5d1986b5eb6c3e7197e63
tom-stephen <tom.stephen@ucalgary.ca>
2023-02-23 19:11:54 -0700

    adding some code for the adding note funcion. it is deffinitly in the wrong place and not done but a starting place.

--- src/App.js
+++ src/App.js
@@ -45,7 +45,38 @@ function App() {
         </div>
         {/* right side */}
         <div id="right-side">
-          <h1>Select a note, or create a new one.</h1>
+          {/* <h1>Select a note, or create a new one.</h1> */}
+
+{/* ADDING TEST HERE */}
+          {/* bar to put title in */}
+          <div id="note-header-box">
+            <div id="note-title-box">
+              <input id="note-title" type="text" placeholder="Untitled"></input>
+              <input id="note-date"type="datetime-local" />
+            </div>
+
+            <div id="note-header-buttons">
+              <button id="save">Save</button>
+              <button id="delete">Delete</button>
+            </div>
+          </div>
+
+          {/* making note tools */}
+          <div id="note-tools">
+            <select onchange="changeFontSize(this)">
+              <option value="1" id="size-normal">normal</option>
+              <option value="2" id="size-subheader">Subheader</option>
+              <option value="3" id="size-header">Header</option>
+            </select>
+            <button id="bold">B</button>
+            <button id="italic">I</button>
+            <button id="underline">U</button>
+            <button id="link">&#128279;</button>
+            <button id="bulleted-list">Bulleted List</button>
+            <button id="numbered-list">Numbered List</button>
+          </div>
+
+{/* ADDING TEST HERE */}
         </div>
       </div>
     </div>
--- src/edit.js
+++ src/edit.js
@@ -1,4 +1,10 @@
 
 function edit(){
-    
+    return(
+        <div id="note-title-box">
+            <input id="note-title" type="text" placeholder="Untitled"></input>
+        </div>
+    )
 }
+
+export default edit;
