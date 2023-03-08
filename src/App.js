0cc29a907762db304b6c7d69a81d84dcb709772f
tom-stephen <tom.stephen@ucalgary.ca>
2023-03-08 15:44:03 -0700

    changint the name of edit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState , useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import Edit from './Edit';
+import Edit from './edit';
 import NoteListPreview from './NoteListPreview';
 import { Outlet } from "react-router-dom";
 
